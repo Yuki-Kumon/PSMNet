502503792bdd9c50a97f26547d09452bab120aaa
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-11-15 18:12:36 +0900

    akirameta...

--- detect.py
+++ detect.py
@@ -73,7 +73,6 @@ def main(_argv):
         psmnet = psmnet.to('cuda')
         criterion = criterion.to('cuda')
         logging.info('use cuda')
-        psmnet = nn.DataParallel(psmnet)
     else:
         logging.info('NOT use cuda')
 
