ec39f57b1f2df04907ff87080e882f80c09dbaaf
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-11-15 18:59:55 +0900

    fix bugs

--- detect.py
+++ detect.py
@@ -144,7 +144,7 @@ def detect(batch, loader, model, criterion, is_cuda, loop_max=-1):
                 disp3 = disp3.to('cpu')
                 target_disp = target_disp.to('cpu')
             output_list.append(disp3.detach().numpy()[0, 0])
-            GT_list.append(target_disp.numpy()[0])
+            GT_list.append(target_disp.numpy()[0, 0])
             image_list.append(left_img.numpy()[0])
 
             if is_cuda:
